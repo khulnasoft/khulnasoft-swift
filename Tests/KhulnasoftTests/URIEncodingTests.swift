cd71dbc05d37b4775ba9aa18ece2649934648a3f
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-03-19 12:02:27 +0600

    Create URIEncodingTests.swift
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ Tests/KhulnasoftTests/URIEncodingTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+@testable import Khulnasoft
+
+final class URIEncodingTests: XCTestCase {
+    func testDataURIEncoding() throws {
+        let string = "Hello, World!"
+        let base64Encoded = "SGVsbG8sIFdvcmxkIQ=="
+        XCTAssertEqual(string.data(using: .utf8)?.base64EncodedString(), base64Encoded)
+
+        let mimeType = "text/plain"
+        XCTAssertEqual(string.data(using: .utf8)?.uriEncoded(mimeType: mimeType),
+                       "data:\(mimeType);base64,\(base64Encoded)")
+    }
+
+    func testISURIEncoded() throws {
+        XCTAssertTrue(Data.isURIEncoded(string: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ=="))
+        XCTAssertFalse(Data.isURIEncoded(string: "Hello, World!"))
+        XCTAssertFalse(Data.isURIEncoded(string: ""))
+    }
+
+    func testDecodeURIEncoded() throws {
+        let encoded = "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ=="
+        guard case let (mimeType, data)? = Data.decode(uriEncoded: encoded) else {
+            return XCTFail("failed to decode data URI")
+        }
+
+        XCTAssertEqual(mimeType, "text/plain")
+        XCTAssertEqual(String(data: data, encoding: .utf8), "Hello, World!")
+    }
+}
